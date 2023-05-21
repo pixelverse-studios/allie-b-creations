82ba65e659053f758c11d336f1674e429c2343cd
Phil Arfuso <arfusop@gmail.com>
2023-05-21 13:36:14 -0400

    startings of home

--- components/auth/StyledAuthWrapper.tsx
+++ components/auth/StyledAuthWrapper.tsx
@@ -1,29 +1,26 @@
 import styled from '@emotion/styled'
 
 const StyledAuthWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: auto 1fr auto;
-    grid-template-areas:
-        'header'
-        'main'
-        'footer';
+    display: flex;
+    flex-direction: column;
     height: 100%;
 
     nav {
-        grid-area: header;
+        height: var(--nav-height);
+        position: absolute;
+        width: 100%;
     }
 
     main {
-        grid-area: main;
         margin: 0 auto;
+        margin-top: var(--nav-height);
         width: 100vw;
         max-width: var(--max-width);
         padding: 1rem;
+        height: 100%;
     }
 
     footer {
-        grid-area: footer;
     }
 `
 export default StyledAuthWrapper
---
+++ views/home/index.tsx
@@ -0,0 +1,13 @@
+import img1 from '@/assets/cheat-sheet.png'
+import img2 from '@/assets/contact picture.png'
+
+const Home = () => {
+    return (
+        <section>
+            <div>paralax 1</div>
+            <div className="testimonials">testimonials</div>
+        </section>
+    )
+}
+
+export default Home
