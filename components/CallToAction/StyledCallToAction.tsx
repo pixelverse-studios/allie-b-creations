f9c0caf080bd371a435c0e3f0c355032741e5f04
Kevin <kevin.lacarrubba@yahoo.com>
2023-06-01 09:31:46 -0400

    added in and styled speed dial

--- components/CallToAction/StyledCallToAction.tsx
+++ components/CallToAction/StyledCallToAction.tsx
@@ -1,3 +1,17 @@
 import styled from '@emotion/styled'
 
-export const StyledCallToAction = styled.div``
+export const StyledCallToAction = styled.div`
+    .MuiSpeedDial-fab {
+        background-color: var(--accent-color-2);
+        color: var(--off-brand-dark);
+        &:hover {
+            background-color: var(--accent-color-2);
+        }
+    }
+    #SpeedDialCTA-actions {
+        button {
+            background-color: var(--accent-color-2);
+            color: var(--off-brand-dark);
+        }
+    }
+`
--- components/CallToAction/index.tsx
+++ components/CallToAction/index.tsx
@@ -1,18 +1,32 @@
 import { StyledCallToAction } from './StyledCallToAction'
 
 import { SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material'
+import RateReviewIcon from '@mui/icons-material/RateReview'
+import MessageIcon from '@mui/icons-material/Message'
 
+const SpeedDialOptions = [
+    { icon: <MessageIcon />, name: 'Contact Me' },
+    { icon: <RateReviewIcon />, name: 'Create a Testimonial' }
+]
 const CallToActionButton = () => {
     return (
         <StyledCallToAction>
             <SpeedDial
                 ariaLabel="SpeedDial CTA"
                 sx={{
-                    position: 'absolute',
+                    position: 'fixed',
                     bottom: 20,
                     right: 20
                 }}
-                icon={<SpeedDialIcon />}></SpeedDial>
+                icon={<SpeedDialIcon />}>
+                {SpeedDialOptions.map(option => (
+                    <SpeedDialAction
+                        key={option.name}
+                        icon={option.icon}
+                        tooltipTitle={option.name}
+                    />
+                ))}
+            </SpeedDial>
         </StyledCallToAction>
     )
 }
