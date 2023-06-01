e8eff265771aaf4af89ac36095bdcfbdf005593b
Kevin <kevin.lacarrubba@yahoo.com>
2023-06-01 13:56:16 -0400

    fuck it again

--- components/drawer/StyledDrawer.tsx
+++ components/drawer/StyledDrawer.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 
-export const StyledIcon = styled.span`
+export const StyledIcon = styled.div`
     width: 100%;
     height: 1rem;
     padding: 0.5rem 0.5rem 0 0;
