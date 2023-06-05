64c2072a8b62fd33a435ad7fa1c27fea70d01205
Kevin <kevin.lacarrubba@yahoo.com>
2023-06-05 13:54:48 -0400

    fixed transition

--- components/drawer/index.tsx
+++ components/drawer/index.tsx
@@ -13,27 +13,13 @@ const FormDrawer = () => {
     const { content, showing, title } = useSelector(
         (state: any) => state.drawer
     )
-    if (content === TESTIMONIAL)
-        return (
-            <Drawer
-                anchor="right"
-                open={showing}
-                PaperProps={{ style: { width: '50%' } }}>
-                <StyledDrawerContent>
-                    <StyledDrawerHeader>
-                        <span>{title}</span>
-                        <Close
-                            className="close-icon"
-                            onClick={() => dispatch(closeDrawer())}
-                        />
-                    </StyledDrawerHeader>
-                    <TestimonialForm />
-                </StyledDrawerContent>
-            </Drawer>
-        )
-    if (content === CLIENT_REQUEST)
-        return (
-            <Drawer anchor="right" open={showing}>
+
+    return (
+        <Drawer
+            anchor="right"
+            open={showing}
+            PaperProps={{ style: { width: '50%' } }}>
+            <StyledDrawerContent>
                 <StyledDrawerHeader>
                     <span>{title}</span>
                     <Close
@@ -41,9 +27,10 @@ const FormDrawer = () => {
                         onClick={() => dispatch(closeDrawer())}
                     />
                 </StyledDrawerHeader>
-                {content}
-            </Drawer>
-        )
+                {content === TESTIMONIAL ? <TestimonialForm /> : '{ content }'}
+            </StyledDrawerContent>
+        </Drawer>
+    )
 }
 
 export default FormDrawer
