0650453286c4638b2d0d9715c0146494d949a307
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2023-06-24 01:06:23 -0400

    finito la musica

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,18 +1,22 @@
-import LogoImg from '../../assets/logo.svg'
-
+import { useSelector } from 'react-redux'
 import { Instagram, LinkedIn, MailOutline } from '@mui/icons-material'
+import { SocialMenuItems } from '@/utils/IconSelection'
+import LogoImg from '../../assets/logo.svg'
 import { StyledFooter } from './StyledFooter'
 
 const Footer = () => {
+    const contactLinks = useSelector((state: any) => state.contactLinks)
     return (
         <StyledFooter>
             <img src={LogoImg.src} alt="Logo" className="logo" />
             <div className="social-icons">
-                <Instagram className="icon" />
-                <LinkedIn className="icon" />
-                <MailOutline className="icon" />
+                {contactLinks?.map((link: any) => {
+                    const iconValue = SocialMenuItems.find(
+                        option => option.value === link?.label
+                    )
+                    return iconValue?.icon
+                })}
             </div>
-
             <small>&copy; ABC LLC 2023</small>
         </StyledFooter>
     )
