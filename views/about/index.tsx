7787ef40e80d6c931c01e5b076aeb88c0e37b473
Kevin <kevin.lacarrubba@yahoo.com>
2023-06-28 14:55:16 -0400

    fetched and set an element for all db fields with appropriate classnames

--- views/about/index.tsx
+++ views/about/index.tsx
@@ -5,13 +5,23 @@ const AboutPageView = () => {
     const aboutPageData = useSelector((state: any) => state.aboutPage)
     const { backgroundInfo, header, profileImg, role, subHeader, title } =
         aboutPageData
-
+    console.log(aboutPageData)
     return (
         <StyledAboutPage>
-            <h1 className="header">{header}</h1>
+            <h1 className="title">{title}</h1>
             <div className="image-about-content">
                 <img src={profileImg} alt="alt text" className="image" />
-                <div className="about">h2</div>
+                <div className="about">
+                    <h2 className="header">{header}</h2>
+                    <h3 className="subHeader">{subHeader}</h3>
+                    <div>
+                        {backgroundInfo.map((info: string) => (
+                            <p className="background-info">{info}</p>
+                        ))}
+                    </div>
+                    <span className="role">{role}</span>
+                    <span className="name">The one and only</span>
+                </div>
             </div>
         </StyledAboutPage>
     )
